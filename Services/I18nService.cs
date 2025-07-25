abb72f80c8c07772abdfc577b8be28d917f5160f
ErenYalc1n <rendoom16@hotmail.com>
2025-07-25 03:13:23 +0300

    /blog ve /project sayfaları hazırlandı

--- Services/I18nService.cs
+++ Services/I18nService.cs
@@ -36,5 +36,6 @@ namespace ErenYalcinPortfoy.Services
             Lang = lang;
             OnLanguageChanged?.Invoke();
         }
+
     }
 }
--- wwwroot/js/i18n.js
+++ wwwroot/js/i18n.js
@@ -5,3 +5,23 @@
 window.setLang = function (lang) {
     localStorage.setItem("lang", lang);
 };
+
+window.setMeta = (title, description, url = window.location.href) => {
+    document.title = title;
+
+    const setOrUpdate = (name, attr, value) => {
+        let element = document.querySelector(`${name}`);
+        if (!element) {
+            element = document.createElement("meta");
+            document.head.appendChild(element);
+        }
+        element.setAttribute(attr, value);
+    };
+
+    setOrUpdate("meta[name='description']", "content", description);
+    setOrUpdate("meta[property='og:title']", "content", title);
+    setOrUpdate("meta[property='og:description']", "content", description);
+    setOrUpdate("meta[property='og:url']", "content", url);
+};
+
+
